6562961793ae7b04a2125e2788f4c50d902abdcc
Ashwin <ashwin06k@gmail.com>
2018-09-19 12:14:21 +0530

    Add files via upload

---
+++ TestRandomCashFlow.java
@@ -0,0 +1,35 @@
+
+import java.util.Iterator;
+import java.util.List;
+import com.services.*;
+import com.utilities.RandGenerator;
+import com.beans.*;
+
+public class TestRandomCashFlow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RandGenerator r = new RandGenerator(30, 100, 1000);
+		List<CashFlow> cashflow = r.GenerateRandomCashFlow();
+		
+		Iterator ir=cashflow.iterator();
+		while (ir.hasNext()) 
+			System.out.println(ir.next());
+		
+		Service service=new ServiceImpl();
+		service.addRandomCashflow(cashflow);
+		
+		
+//		Random r = new Random();
+//		double min = -1.00;
+//		double max = 1.00;
+//		
+//		for(int i=0;i<15;i++) {
+//		double rate = min + (max - min) * r.nextDouble();
+//		rate = Math.round(rate * 100.0) / 100.0;
+//		System.out.println(rate);
+	
+		}
+}
+
